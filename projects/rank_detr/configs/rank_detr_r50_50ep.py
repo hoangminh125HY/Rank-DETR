0371975df11e2933c3edfbcb31a469adc902e8de
Apple <apple@MacBook-Pro-cua-Minh.local>
2025-08-30 00:19:01 +0700

    Initial commit

--- projects/rank_detr/configs/rank_detr_r50_50ep.py
+++ projects/rank_detr/configs/rank_detr_r50_50ep.py
@@ -1,7 +1,7 @@
 from detrex.config import get_config
 from .models.rank_detr_r50 import model
 
-dataloader = get_config("/kaggle/working/Rank-DETR/instances_train.json").dataloader
+dataloader = get_config("/Rank-DETR-main/datasets/register_coco_detection.py").dataloader
 lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_50ep
 optimizer = get_config("common/optim.py").AdamW
 train = get_config("common/train.py").train
