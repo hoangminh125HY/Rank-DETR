ede5a383e4a4e131990b7eec1d5f5aafa3889c3b
Apple <apple@MacBook-Pro-cua-Minh.local>
2025-08-30 13:52:18 +0700

    Initial commit

--- detrex/data/datasets/register_my_dataset.py
+++ detrex/data/datasets/register_my_dataset.py
@@ -6,9 +6,9 @@ def register_my_dataset():
     Đăng ký dataset custom theo format COCO (train.json, val.json)
     """
     # Thay đường dẫn theo chỗ bạn để dataset
-    train_json = "/kaggle/working/dataset/annotations/train.json"
-    val_json = "/kaggle/working/dataset/annotations/val.json"
-    img_dir = "/kaggle/working/dataset/images"
+    train_json = "/kaggle/working/annotations/train.json"
+    val_json = "/kaggle/working/annotations/val.json"
+    img_dir = "/kaggle/input/data-private-bus-car-truck/Private_DTS/Images/Images"
 
     # Register train
     register_coco_instances("my_dataset_train", {}, train_json, img_dir)
