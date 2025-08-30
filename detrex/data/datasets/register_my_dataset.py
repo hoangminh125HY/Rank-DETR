2f86a2ac5aea8df16035b4062e15ee399f925899
Apple <apple@MacBook-Pro-cua-Minh.local>
2025-08-30 18:19:53 +0700

    Initial commit

--- detrex/data/datasets/register_my_dataset.py
+++ detrex/data/datasets/register_my_dataset.py
@@ -1,22 +1,20 @@
 from detectron2.data.datasets import register_coco_instances
-from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.data import MetadataCatalog
 
 def register_my_dataset():
     """
     Đăng ký dataset custom theo format COCO (train.json, val.json)
     """
-    # Thay đường dẫn theo chỗ bạn để dataset
     train_json = "/kaggle/working/annotations/train.json"
-    val_json = "/kaggle/working/annotations/val.json"
-    img_dir = "/kaggle/input/data-private-bus-car-truck/Private_DTS/Images/Images"
+    val_json   = "/kaggle/working/annotations/val.json"
+    img_dir    = "/kaggle/input/data-private-bus-car-truck/Private_DTS/Images/Images"
 
-    # Register train
+    # Register datasets
     register_coco_instances("my_dataset_train", {}, train_json, img_dir)
-    # Register val
     register_coco_instances("my_dataset_val", {}, val_json, img_dir)
 
-    # (Optional) metadata
-    MetadataCatalog.get("my_dataset_train").thing_classes = ["Bus","Car","Person"]
-    MetadataCatalog.get("my_dataset_val").thing_classes = ["Bus","Car","Person"]
+    # Set metadata
+    MetadataCatalog.get("my_dataset_train").set(thing_classes=["Bus","Car","Person"])
+    MetadataCatalog.get("my_dataset_val").set(thing_classes=["Bus","Car","Person"])
 
     print("✅ Registered my_dataset_train and my_dataset_val successfully!")
