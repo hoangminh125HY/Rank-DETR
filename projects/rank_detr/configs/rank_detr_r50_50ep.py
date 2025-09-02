4b398a1e2a6276a47b9e8e11bdf2aa69517cca75
Apple <apple@MacBook-Pro-cua-Minh.local>
2025-09-02 23:44:31 +0700

    Initial commit

--- projects/rank_detr/configs/rank_detr_r50_50ep.py
+++ projects/rank_detr/configs/rank_detr_r50_50ep.py
@@ -19,16 +19,16 @@ train.init_checkpoint = "detectron2://ImageNetPretrained/torchvision/R-50.pkl"
 train.output_dir = "./output/rank_detr_r50_50ep"
 
 # max training iterations
-train.max_iter = 34600
+train.max_iter = 12500
 
 # run evaluation every 5000 iters
-train.eval_period = 5000
+train.eval_period = 2500
 
 # log training infomation every 20 iters
 train.log_period = 20
 
 # save checkpoint every 5000 iters
-train.checkpointer.period = 5000
+train.checkpointer.period = 2500
 
 # gradient clipping for training
 train.clip_grad.enabled = True
@@ -46,12 +46,12 @@ optimizer.weight_decay = 1e-4
 optimizer.params.lr_factor_func = lambda module_name: 0.1 if "backbone" in module_name else 1
 
 # modify dataloader config
-dataloader.train.num_workers = 2
+dataloader.train.num_workers = 8
 
 # please notice that this is total batch size.
 # surpose you're using 4 gpus for training and the batch size for
 # each gpu is 16/4 = 4
-dataloader.train.total_batch_size = 2
+dataloader.train.total_batch_size = 8
 
 # dump the testing results into output_dir for visualization
 dataloader.evaluator.output_dir = train.output_dir
